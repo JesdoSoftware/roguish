b6c51f3c7f77bd5b8f1a5745ee185d492800af76
Jesse Douglas <jesse@jesdosoftware.com>
2023-03-05 20:27:42 -0500

    Add ESLint Node env

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,8 @@ You should have received a copy of the GNU General Public License along with
 this program. If not, see <https://www.gnu.org/licenses/>.
 */
 
+/* eslint-disable @typescript-eslint/no-var-requires */
+
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
